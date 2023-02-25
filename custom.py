1864c2dacde8df439e444a654eb9167d5b4770b2
Oliver Green <ojgreensemail@gmail.com>
2023-02-25 15:06:50 +0000

    Built loop to take pictures and organise the files

--- custom.py
+++ custom.py
@@ -8,19 +8,21 @@ camera_config = picam2.create_still_configuration(transform=Transform(vflip=True
 picam2.configure(camera_config)
 
 
-def take_picture():
+def take_picture(fileName):
     picam2.start()
     time.sleep(2)
-    picam2.capture_file("test.jpg")
+    picam2.capture_file(fileName)
     picam2.stop()
 
 
-take_picture()
+# take_picture()
 
-# while True:
-#
-#     take_picture()
-#     time.sleep(2100)
-#
-#     # If statement that will end the loop once finished
-#     if time
+# Set up loop conditions
+pictureNumber = 0
+
+while True:
+
+    take_picture(f'/timelapse/Picture{pictureNumber}.jpg')
+    time.sleep(300)
+    pictureNumber += 1
+
