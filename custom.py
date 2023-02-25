55e1e1f73f3136c9ac42c7dcb7200073a5e53792
Oliver Green <ojgreensemail@gmail.com>
2023-02-25 14:48:27 +0000

    Refactored into function, changed config

--- custom.py
+++ custom.py
@@ -1,9 +1,26 @@
 from picamera2 import Picamera2
 import time
 
+from libcamera import Transform
+
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
+camera_config = picam2.create_still_configuration(transform=Transform(vflip=True))
 picam2.configure(camera_config)
-picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+
+
+def take_picture():
+    picam2.start()
+    time.sleep(2)
+    picam2.capture_file("test.jpg")
+    picam2.stop()
+
+
+take_picture()
+
+# while True:
+#
+#     take_picture()
+#     time.sleep(2100)
+#
+#     # If statement that will end the loop once finished
+#     if time
