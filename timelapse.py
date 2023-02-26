05dd5fcd057bff9acc3f8cebd12a84033a456f63
Oliver Green <ojgreensemail@gmail.com>
2023-02-26 16:12:15 +0000

    Update timelapse.py
    Introduced high-level function

--- timelapse.py
+++ timelapse.py
@@ -10,9 +10,7 @@ picam2.configure(camera_config)
 
 
 def take_picture(filename):
-    picam2.start()
-    time.sleep(0.5)
-    picam2.capture_file(filename)
+    picam2.start_and_capture_file(filename)
     picam2.stop()
 
 
